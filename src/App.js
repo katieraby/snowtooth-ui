da38c407b9d043bf8e1b6465a1a3e48aa064cca2
Katie <50029859+katieraby@users.noreply.github.com>
2020-05-13 18:59:11 +0100

    Added subscription, mutation and queries for graphQL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,78 @@
 import React from "react";
 import "./App.css";
+import { gql, useQuery, useMutation, useSubscription } from "@apollo/client";
+import { StatusIndicator } from "./StatusIndicator";
+
+const QUERY = gql`
+  query {
+    allLifts {
+      id
+      name
+      status
+    }
+  }
+`;
+
+const MUTATION = gql`
+  mutation($status: LiftStatus!, $id: ID!) {
+    setLiftStatus(status: $status, id: $id) {
+      id
+      name
+      status
+    }
+  }
+`;
+
+const SUBSCRIPTION = gql`
+  subscription {
+    liftStatusChange {
+      id
+      status
+    }
+  }
+`;
 
 function App() {
-  return <div>Build something cool!</div>;
+  const { loading, data } = useQuery(QUERY);
+  const [setStatus] = useMutation(MUTATION);
+  useSubscription(SUBSCRIPTION);
+  if (loading) return <p>Loading lifts...</p>;
+
+  return (
+    <section>
+      <h1>Snowtooth Lift Status</h1>
+      {data && !loading && (
+        <table>
+          <thead>
+            <tr>
+              <th>Lift Name</th>
+              <th>Current Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allLifts.map((lift) => (
+              <tr>
+                <td>{lift.name}</td>
+                <td>
+                  <StatusIndicator
+                    status={lift.status}
+                    onChange={(status) =>
+                      setStatus({
+                        variables: {
+                          id: lift.id,
+                          status,
+                        },
+                      })
+                    }
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </section>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { render } from "react-dom";
 import App from "./App";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+  split,
+} from "@apollo/client";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const httpLink = new HttpLink({
+  uri: "https://snowtooth.moonhighway.com",
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://snowtooth.moonhighway.com/graphql`,
+  options: {
+    reconnect: true,
+    lazy: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({ link, cache });
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
